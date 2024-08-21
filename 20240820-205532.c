fed61c128b1776c63bc007afdf43cc1269ef7021
yousaren <2290716570@qq.com>
2024-08-21 19:15:49 +0800

    Update 20240820-205532.c

--- 20240820-205532.c
+++ 20240820-205532.c
@@ -3,21 +3,21 @@
 using namespace std;
 
 int main() {
-    // 定义变量
+    
     double num1, num2, sum;
+    // 定义变量
 
-    // 提示用户输入两个数字
     cout << "Enter the first number: ";
     cin >> num1;
-
+	
     cout << "Enter the second number: ";
     cin >> num2;
-
-    // 计算加法
+	//输入两个数字
+   
     sum = num1 + num2;
-
-    // 输出结果
+	// 计算加法
+    
     cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
-
+	// 输出结果
     return 0;
 }
